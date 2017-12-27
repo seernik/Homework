4d1dff5ccb7f62cd669a68a612a2ab2d65782a97
seernik <34370781+seernik@users.noreply.github.com>
2017-12-27 07:35:00 +0200

    upd

---
+++ com/company/lesson5/Arrays12.java
@@ -0,0 +1,12 @@
+package com.company.lesson5;
+
+import java.util.Arrays;
+
+public class Arrays12 {
+    public static void main(String[] args) {
+        char[][] ch = new char[][]{{'a', 'b'}, {'c', 'd'}, {'f', 'g'}, {'e', 'h'}};
+        System.out.println(Arrays.deepToString(ch));
+    }
+}
+
+
